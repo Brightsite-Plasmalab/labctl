1acfa561378ac5696f6f7798b976a4a5eaa81136
Martijn Ruijzendaal <martijn.ruijzendaal@gmail.com>
2025-02-13 10:15:19 +0100

    Add template class for classes inheriting BaseCameraExperiment

---
+++ src/labctl/experiments/template.py
@@ -0,0 +1,40 @@
+from abc import abstractmethod
+from typing import List
+from labctl.script import Script
+from labctl.experiments.base_camera_experiment import BaseCameraExperiment
+
+
+class TemplateExperiment(BaseCameraExperiment):
+
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+    @abstractmethod
+    def get_config_names(self) -> List[str]:
+        """Get the human-readable names of the configurations."""
+        pass
+
+    def prepare_experiment(self, cmds: Script):
+        """Prepare the experiment. Inherit this method to add more commands."""
+        pass
+
+    @abstractmethod
+    def prepare_config(self, cmds, i):
+        """Prepares experimental configuration i."""
+        pass
+
+    def get_camera_delay(self, config, version):
+        """Get the camera delay for a specific configuration, frame, and version."""
+        if version == 0:
+            # foreground
+            return self.camera_delay_optimum
+        else:
+            # background
+            return self.camera_delay_background
+
+    def shutdown_experiment(self):
+        """Shutdown the experiment. Inherit this method to add more commands."""
+        pass
