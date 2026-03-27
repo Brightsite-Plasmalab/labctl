439368d4e1117d6effbbeb64f41e6250cd597b5e
Martijn Ruijzendaal <martijn.ruijzendaal@gmail.com>
2026-03-27 17:04:15 +0100

    rework combined polarisation + translation stage experiment

--- src/labctl/experiments/polarised_translation_stage.py
+++ src/labctl/experiments/polarised_translation_stage.py
@@ -1,6 +1,4 @@
 from typing_extensions import List
-from labctl.devices import PiTranslationStage
-from labctl.script import Script
 from labctl.experiments.polarisation import PolarisationFilterExperiment
 from labctl.experiments.translation_stage import TranslationStageExperiment
 
@@ -8,42 +6,32 @@ from labctl.experiments.translation_stage import TranslationStageExperiment
 class PolarisedTranslationStageExperiment(
     PolarisationFilterExperiment, TranslationStageExperiment
 ):
-    def __init__(self, **kwargs):
-        N_x = len(kwargs.get("x", []))
-        N_pol = 2
-
-        # Make sure there are N_x * N_pol configurations.
-        # If there are just N_x configurations, repeat each N_pol times.
-
-        if len(kwargs.get("N_frames", [])) == N_x:
-            kwargs["N_frames"] = [n for n in kwargs["N_frames"] for _ in range(N_pol)]
-        assert (
-            len(kwargs.get("N_frames", [])) == N_x * N_pol
-        ), "N_frames should have length N_x * N_pol"
-
-        super().__init__(**kwargs)
-        # TranslationStageExperiment.__init__(self, x, **kwargs)
-        # PolarisationFilterExperiment.__init__(
-        #     self, alpha_ver=alpha_ver, alpha_hor=alpha_hor, **kwargs
-        # )
+    """
+    This experiment combines the capabilities of the PolarisationFilterExperiment and the TranslationStageExperiment.
+    It will run through all combinations of the translation stage positions and polarisation filter angles.
+    """
 
     def get_config_names(self) -> List[str]:
         config_names_translation = TranslationStageExperiment.get_config_names(self)
         config_names_polarisation = PolarisationFilterExperiment.get_config_names(self)
-        print(config_names_polarisation, config_names_translation)
         return [
             f"{cn_t}_{cn_p}"
             for cn_t in config_names_translation
             for cn_p in config_names_polarisation
         ]
 
-    def make_labctl_header(self):
-        return super().make_labctl_header()
-        # super(PolarisationFilterExperiment, self).make_labctl_header()
+    def check_N_frames(self, expected_length, config_explanation):
+        N_x = len(self.x)
+        N_pol = 2
 
-    def prepare_experiment(self, cmds: Script):
-        super(TranslationStageExperiment, self).prepare_experiment(cmds)
-        super(PolarisationFilterExperiment, self).prepare_experiment(cmds)
+        # Make sure there are N_x * N_pol configurations.
+        # If there are just N_x configurations, repeat each N_pol times.
+
+        if len(self.n_frames) == N_x:
+            self.n_frames = [n for n in self.n_frames for _ in range(N_pol)]
+        assert (
+            len(self.n_frames) == N_x * N_pol
+        ), "N_frames should have length N_x * N_pol"
 
     def prepare_config(self, cmds, i):
         super(TranslationStageExperiment, self).prepare_config(cmds, i)
