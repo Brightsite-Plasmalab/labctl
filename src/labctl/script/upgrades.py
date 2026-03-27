8336ffe110f688e9df6143c51147329ab4bec051
Martijn Ruijzendaal <martijn.ruijzendaal@gmail.com>
2026-03-27 11:52:56 +0100

    improve device registration logic

--- src/labctl/script/upgrades.py
+++ src/labctl/script/upgrades.py
@@ -48,16 +48,46 @@ class DeviceCommands(ScriptBase):
     current_channel: int = -1
     devices: dict[DeviceBase, int] = {}
 
-    def register_device(self, device, channel):
+    def register_device(self, device, channel=None):
+        if channel is None:
+            unoccupied_channels = set(range(1, 4)) - set(self.devices.values())
+            if len(unoccupied_channels) == 0:
+                raise Exception("No unoccupied channels available")
+            channel = min(unoccupied_channels)
+
+        if device in self.devices:
+            raise Exception(
+                f"Device {device} is already registered on channel {self.devices[device]}"
+            )
+        if channel in self.devices.values():
+            raise Exception(
+                f"Channel {channel} is already occupied by device {self.get_device_by_channel(channel).__class__.__name__}"
+            )
+
+        print(f"Registering device {device.__class__.__name__} on channel {channel}")
         self.devices[device] = channel
+
+    def get_device_channel(self, device):
+        if device not in self.devices:
+            raise Exception(f"Device {device} is not registered")
+        return self.devices[device]
+
+    def get_device_by_channel(self, channel):
+        for device, ch in self.devices.items():
+            if ch == channel:
+                return device
+        raise Exception(f"No device registered on channel {channel}")
 
     def is_registered(self, device):
         return device in self.devices
 
     def switch_device(self, channel):
         if isinstance(channel, DeviceBase):
-            self.switch_device(self.devices[channel])
+            self.switch_device(self.get_device_channel(channel))
         elif type(channel) == int:
+            assert (
+                self.get_device_by_channel(channel) is not None
+            ), f"No device registered on channel {channel}"
             if self.current_channel != channel:
                 self.append(f"#SELSER {channel}")
                 self.current_channel = channel
